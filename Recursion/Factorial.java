9c93dc0bb445322ae8e4c7b28260d3b0819a450e
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-12-18 18:14:58 +0530

    add

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,12 +1,26 @@
 import java.util.*;
 class Factorial{
+  // functional method
   public static int findFactorial(int n){
     if(n==1) return 1;
     return n*findFactorial(n-1);
   }
+
+  // parameterized method
+
+  public static void fact(int n,int currFact){
+    if(n==1){
+      System.out.print(currFact); return;
+    }
+    fact(n-1 , currFact*n);
+
+
+  }
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-    System.out.println(findFactorial(n));
+    // System.out.println(findFactorial(n));
+
+    fact(n,1);
   }
 }
