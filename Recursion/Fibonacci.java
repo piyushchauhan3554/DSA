d079aff81b04cf9d0fb8488f53ff5220559215ff
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-12-21 12:44:06 +0530

    add

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class Fibonacci{
+  // using loop
   public static int nthFibLoop(int n){
     if(n==0) return 0;
     if(n==1) return 1;
@@ -12,11 +13,18 @@ class Fibonacci{
 
     return fib[n];
   }
+
+  // using recursion
+  public static int nthFibRecursion(int n){
+    if(n<=1) return n; // n==0 || n==1 return n
+    return nthFibRecursion(n-1)+nthFibRecursion(n-2);
+  }
+
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     System.out.println("enter n");
     int n=sc.nextInt();
 
-    System.out.print(nthFibLoop(n));
+    System.out.print(nthFibRecursion(n));
   }
 }
